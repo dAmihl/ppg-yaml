b33b33c7a861efa78a1415af99964c9ba24fc0b8
dAmihl <m.egger@e-mx.at>
2020-05-20 18:13:57 +0200

    improved cmake build

--- create_includes.sh
+++ create_includes.sh
@@ -4,4 +4,5 @@ mkdir -p include
 mkdir -p lib
 cp src/*.h include
 cp build/Release/*.lib lib
-cp includes/ppg-core/lib/ppg-core.lib lib
+cp build/includes/thirdparty/yaml-cpp/Release/*.lib lib
+cp build/includes/ppg-core/build_results/bin/Release/*.lib lib
