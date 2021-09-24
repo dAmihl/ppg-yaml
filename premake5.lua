740b3868b62bba53b127cac99927247e3cc4a315
dAmihl <m.egger@e-mx.at>
2021-09-24 21:37:02 +0200

    first cleanup

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+workspace "PPG-YAML"
+	language "C++"
+	cppdialect "C++14"
+	configurations { "Debug", "Release"}
+	
+project "PPG-YAML"
+	kind "StaticLib"
+	language "C++"
+	targetdir "bin/%{cfg.buildcfg}"
+	
+	files {"src/**.h", "src/**.cpp"}
+	
+	includedirs {"vendor/ppg-core/src", "vendor/yaml-cpp/include"}
+	
+	filter "configurations:Debug"
+		defines {"DEBUG"}
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines {"NDEBUG"}
+		symbols "On"
+			
+--------------- Example
+project ("PPG-YAML-example")
+	location "example"
+	targetdir "bin"
+	kind "ConsoleApp"
+	files {"example/*.cpp"}
+	includedirs {"src"}
+	links {"PPG-YAML"}
+	
+	filter "configurations:Debug"
+		defines {"DEBUG"}
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines {"NDEBUG"}
+		symbols "On"
