e3bf6195f6111958bc04fd4dd517ded67e449722
dAmihl <m.egger@e-mx.at>
2021-09-24 22:40:17 +0200

    update ppg-core. added yaml-cpp build to premake w/ linking. example working with sample universe

--- example/example.cpp
+++ example/example.cpp
@@ -1,6 +1,16 @@
+
 #include <iostream>
+
+#include "Yaml2Puzzle.h"
 
 int main()
 {
+
+	Yaml2Puzzle y2p;
+
+	auto P = y2p.generatePuzzleByFile("universe1.yaml");
+
+	std::cout << P->getExtendedTextualRepresentation() << "\n";
+
 	return 0;
 }
--- premake5.lua
+++ premake5.lua
@@ -3,10 +3,31 @@ workspace "PPG-YAML"
 	cppdialect "C++14"
 	configurations { "Debug", "Release"}
 	
+	include "vendor/ppg-core/ppg-core.lua"
+	
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	targetdir "bin/%{cfg.buildcfg}"
+	
+	files{"vendor/yaml-cpp/src/**.h", "vendor/yaml-cpp/src/**.cpp"}
+	
+	includedirs {"vendor/yaml-cpp/include"}
+	
+	filter "configurations:Debug"
+		defines {"DEBUG"}
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines {"NDEBUG"}
+		symbols "On"
+	
 project "PPG-YAML"
 	kind "StaticLib"
 	language "C++"
 	targetdir "bin/%{cfg.buildcfg}"
+	
+	links {"PPG", "yaml-cpp"}
 	
 	files {"src/**.h", "src/**.cpp"}
 	
@@ -26,7 +47,7 @@ project ("PPG-YAML-example")
 	targetdir "bin"
 	kind "ConsoleApp"
 	files {"example/*.cpp"}
-	includedirs {"src", "vendor/ppg-core/src"}
+	includedirs {"src", "vendor/ppg-core/src", "vendor/yaml-cpp/include"}
 	links {"PPG-YAML"}
 	
 	filter "configurations:Debug"
